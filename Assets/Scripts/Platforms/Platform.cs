0e37bf0f40a5447be8b611aafc66b57d7c8f9054
Javier Riera Chirivella <mrvizious@gmail.com>
2020-04-04 17:41:29 +0200

    Hiding platform created

---
+++ Assets/Scripts/Platforms/Platform.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Platform : MonoBehaviour {
+
+}
---
+++ Assets/Scripts/Platforms/PlatformHiding.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformHiding : Platform {
+
+	[SerializeField] private bool activated;
+	private Collider2D collider;
+	private SpriteRenderer sprite;
+
+	// Start is called before the first frame update
+	void Start() {
+		collider = GetComponent<Collider2D>();
+		sprite = GetComponent<SpriteRenderer>();
+		setActivated(activated);
+	}
+
+	public void Activate() {
+		activated = true;
+		collider.enabled = activated;
+		if (debugGraphic) {
+			Color tmp = sprite.color;
+			tmp.a = 1f;
+			sprite.color = tmp;
+		}
+	}
+	public void Deactivate() {
+		activated = false;
+		collider.enabled = activated;
+		if (debugGraphic) {
+			Color tmp = sprite.color;
+			tmp.a = 0.2f;
+			sprite.color = tmp;
+		}
+	}
+	public void ToggleActivate() {
+		setActivated(!activated);
+	}
+
+	public void setActivated(bool newValue) {
+		if (newValue) Activate();
+		else Deactivate();
+	}
+
+	/**********************************************************
+     *                    DEBUGGING                           *
+	 **********************************************************/
+
+	//Changes opacity of the sprites to see if they are activated or not
+	public bool debugGraphic;
+
+	private void Update() {
+		if (debugGraphic && Input.GetKeyDown(KeyCode.C)) {
+			ToggleActivate();
+			Debug.Log("Alpha of sprite color: " + sprite.color.a);
+		}
+	}
+
+}
